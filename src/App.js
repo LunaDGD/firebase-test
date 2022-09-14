de30bfe8f60070f7c014e0603b3321fac550489d
LunaDGD <lunadgd@protonmail.com>
2022-09-14 10:51:21 -0400

    list updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
 
 function App() {
+  const [newName, setNewName] = useState('');
+  const [users, setUsers] = useState([]);
+  const usersCollctionRef = collection(db, 'users');
+
+  const createUser = async () => {
+    await addDoc(usersCollctionRef, { name: newName });
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, 'users', id);
+    await deleteDoc(userDoc);
+  };
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollctionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="name.."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+
+      <button onClick={createUser}>Add Friend</button>
+
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>{user.name}</h1>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              Delete friend
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
